a2ebd06e096663e4d03763020d6862d5fd00d21f
Renzo <renzo@personal.com>
2018-12-09 18:36:31 -0300

    Script calculator in perl was created

---
+++ scripts/calculator.pl
@@ -0,0 +1,26 @@
+#/usr/bin/perl
+
+system(clear);
+print "Welcome\n";
+print "Please insert 2 numbers\n";
+$num1 = <STDIN>;
+$num2 = <STDIN>;
+print " 1- Sum \n 2- Substraction \n 3- Multiplication \n 4- Division \n 0- exit \n";
+$opt = <STDIN>;
+
+if($opt == 1){
+print $num1+$num2;
+}elsif($opt == 2){
+print $num1-$num2;
+}elsif($opt == 3){
+print $num1*$num2;
+}elsif($opt == 4){
+print $num1/$num2;
+}elsif($opt == 0){
+exit;
+}else{
+print "Wrong option \n";
+}
+print "\nPress any button to continue\n";
+$con= <STDIN>;
+
